fe095028f91c5f3711aaa7d91c017310aa18d8ed
VeNoM <kumarsks619@gmail.com>
2020-12-04 17:19:05 +0530

    Updates

---
+++ precache-manifest.faee7dd748c38b784ad462932a4f445b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df088886ec77d5d974e06ab65cc05f27",
+    "url": "/quiz-app/index.html"
+  },
+  {
+    "revision": "0a829f98c7c33e5fea8a",
+    "url": "/quiz-app/static/css/main.4517eb0d.chunk.css"
+  },
+  {
+    "revision": "e78755bfa439d8fb4032",
+    "url": "/quiz-app/static/js/2.41812a74.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quiz-app/static/js/2.41812a74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a829f98c7c33e5fea8a",
+    "url": "/quiz-app/static/js/main.7aee8f5e.chunk.js"
+  },
+  {
+    "revision": "e15a1632907c25963bb7",
+    "url": "/quiz-app/static/js/runtime-main.4f483d45.js"
+  }
+]);
